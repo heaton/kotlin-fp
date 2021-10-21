9aaf92ae5e34fbe7182ef58053a0192696153264
heaton.cai <heaton.cai@myob.com>
2021-10-21 16:53:52 +1100

    more Rational test cases

--- app/src/main/kotlin/com/myob/Rational.kt
+++ app/src/main/kotlin/com/myob/Rational.kt
@@ -5,4 +5,9 @@ data class Rational(val x: Int, val y: Int) {
         return "$x/$y"
     }
 
+    operator fun compareTo(other: Rational): Int {
+        return x * other.y - other.x * y
+    }
+
+    operator fun unaryMinus(): Rational = Rational(-x, y)
 }
--- app/src/test/kotlin/com/myob/RationalSpec.kt
+++ app/src/test/kotlin/com/myob/RationalSpec.kt
@@ -1,16 +1,38 @@
 package com.myob
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.*
+import java.lang.IllegalArgumentException
 
 class RationalSpec : WordSpec({
     "Rational" should {
         "display 1/2" {
             Rational(1, 2).show() shouldBe "1/2"
+            Rational(2, 4).show() shouldBe "1/2"
         }
 
         "display 1 for 1/1" {
             Rational(1, 1).show() shouldBe "1"
+        }
+
+        "compare the rational numbers" {
+            (Rational(1, 2) > Rational(1, 3)) shouldBe true
+            (Rational(1, 3) < Rational(1, 2)) shouldBe true
+            -Rational(1, 2) shouldBe Rational(-1, 2)
+        }
+
+        "support opertions" {
+            Rational(1, 2) + Rational(1, 2) shouldBe Rational(1, 1)
+            Rational(1, 2) - Rational(1, 2) shouldBe Rational(0, 1)
+            Rational(1, 2) * 2 shouldBe Rational(1, 1)
+            Rational(1, 1) / 2 shouldBe Rational(1, 2)
+        }
+
+        "throw error for x/0" {
+            shouldThrow<IllegalArgumentException> {
+                Rational(1, 0)
+            }
         }
     }
 })

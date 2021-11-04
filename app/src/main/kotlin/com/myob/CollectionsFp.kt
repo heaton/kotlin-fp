4d238ba75ba3ee4696ea80eddeb9468cff4e859d
heaton.cai <heaton.cai@myob.com>
2021-11-04 16:49:13 +1100

    make the sum logic generic

--- app/src/main/kotlin/com/myob/CollectionsFp.kt
+++ app/src/main/kotlin/com/myob/CollectionsFp.kt
@@ -1,8 +1,16 @@
 package com.myob
 
+import java.math.BigDecimal
+
 object CollectionsFp {
-    fun sumOf(intList: List<Int>): Int =
-        if(intList.isEmpty()) 0 else intList.first() + sumOf(intList.drop(1))
+    fun sumOf(intList: List<Int>): Int = f(0, { a, b -> a + b }, intList)
+
+    fun sumOf(numList: List<BigDecimal>): BigDecimal = f(BigDecimal(0), { a, b -> a + b }, numList)
+
+    fun sumOf(stringList: List<String>): String = f("", { a, b -> a + b }, stringList)
+
+    fun <T> f(initialValue: T, a: (T, T) -> T, list: List<T>): T =
+        if (list.isEmpty()) initialValue else a(list.first(), f(initialValue, a, list.drop(1)))
 
     fun sumOfOo(intList: List<Int>): Int {
         var s = 0
--- app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
+++ app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
@@ -3,11 +3,20 @@ package com.myob
 import com.myob.CollectionsFp.sumOf
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
 
 class CollectionsFpSpec: WordSpec({
     "Collections" should {
         "sum up a list" {
             sumOf(listOf(1, 2, 3)) shouldBe 6
+        }
+
+        "sum up another list" {
+            sumOf(listOf(BigDecimal(1), BigDecimal(2), BigDecimal(3))) shouldBe BigDecimal(6)
+        }
+
+        "add up a list of strings" {
+            sumOf(listOf("1", "2", "3")) shouldBe "123"
         }
     }
 })

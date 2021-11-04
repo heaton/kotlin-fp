629aa85733b1a103f509243deae7f78b3a26e634
heaton.cai <heaton.cai@myob.com>
2021-11-04 17:03:55 +1100

    refactor a fold method

--- app/src/main/kotlin/com/myob/CollectionsFp.kt
+++ app/src/main/kotlin/com/myob/CollectionsFp.kt
@@ -3,14 +3,17 @@ package com.myob
 import java.math.BigDecimal
 
 object CollectionsFp {
-    fun sumOf(intList: List<Int>): Int = f(0, { a, b -> a + b }, intList)
+    fun sumOf(intList: List<Int>): Int = fold(0, { b, a -> a + b }, intList)
 
-    fun sumOf(numList: List<BigDecimal>): BigDecimal = f(BigDecimal(0), { a, b -> a + b }, numList)
+    fun sumOf(numList: List<BigDecimal>): BigDecimal = fold(BigDecimal(0), { b, a -> a + b }, numList)
 
-    fun sumOf(stringList: List<String>): String = f("", { a, b -> a + b }, stringList)
+    fun sumOf(stringList: List<String>): String = fold("", { b, a -> a + b }, stringList)
 
-    fun <T> f(initialValue: T, a: (T, T) -> T, list: List<T>): T =
-        if (list.isEmpty()) initialValue else a(list.first(), f(initialValue, a, list.drop(1)))
+    fun <T, R> fold(initialValue: R, a: (R, T) -> R, list: List<T>): R =
+        if (list.isEmpty()) initialValue else a(fold(initialValue, a, list.drop(1)), list.first())
+
+    fun doubleOf(intList: List<Int>): List<Int> =
+        fold(emptyList(), { acc, a -> acc + (a * 2) }, intList)
 
     fun sumOfOo(intList: List<Int>): Int {
         var s = 0
--- app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
+++ app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
@@ -1,11 +1,12 @@
 package com.myob
 
+import com.myob.CollectionsFp.doubleOf
 import com.myob.CollectionsFp.sumOf
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 import java.math.BigDecimal
 
-class CollectionsFpSpec: WordSpec({
+class CollectionsFpSpec : WordSpec({
     "Collections" should {
         "sum up a list" {
             sumOf(listOf(1, 2, 3)) shouldBe 6
@@ -17,6 +18,10 @@ class CollectionsFpSpec: WordSpec({
 
         "add up a list of strings" {
             sumOf(listOf("1", "2", "3")) shouldBe "123"
+        }
+
+        "double a list" {
+            doubleOf(listOf(1, 2, 3)) shouldBe listOf(2, 4, 6)
         }
     }
 })

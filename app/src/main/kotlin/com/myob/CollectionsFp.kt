50c95e857b14a4b050714f314e4002a1c902fb53
heaton.cai <heaton.cai@myob.com>
2021-11-18 16:15:02 +1100

    implement map

--- app/src/main/kotlin/com/myob/CollectionsFp.kt
+++ app/src/main/kotlin/com/myob/CollectionsFp.kt
@@ -3,17 +3,24 @@ package com.myob
 import java.math.BigDecimal
 
 object CollectionsFp {
-    fun sumOf(intList: List<Int>): Int = fold(0, { acc, a -> acc + a}, intList)
+    fun sumOf(intList: List<Int>): Int = fold(0, { acc, a -> acc + a }, intList)
 
-    fun sumOf(numList: List<BigDecimal>): BigDecimal = fold(BigDecimal(0), { acc, a -> acc + a}, numList)
+    fun sumOf(numList: List<BigDecimal>): BigDecimal = fold(BigDecimal(0), { acc, a -> acc + a }, numList)
 
     fun sumOf(stringList: List<String>): String = fold("", { acc, a -> acc + a }, stringList)
 
     fun <T, R> fold(acc: R, f: (R, T) -> R, list: List<T>): R =
         if (list.isEmpty()) acc else fold(f(acc, list.first()), f, list.drop(1))
 
-    fun doubleOf(intList: List<Int>): List<Int> =
-        fold(emptyList(), { acc, a -> acc + (a * 2) }, intList)
+    fun doubleOf(intList: List<Int>): List<Int> = map(intList) { it * 2 }
+
+    fun tripleOf(intList: List<Int>): List<Int> = map(intList) { it * 3 }
+
+    private fun <T, R> map(intList: List<T>, f: (T) -> R): List<R> =
+        fold(emptyList(), { acc, a -> acc + f(a) }, intList)
+
+    fun evenOf(list: List<Int>): List<Int> = emptyList()
+    fun oddOf(list: List<Int>): List<Int> = emptyList()
 
     fun sumOfOo(intList: List<Int>): Int {
         var s = 0
--- app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
+++ app/src/test/kotlin/com/myob/CollectionsFpSpec.kt
@@ -1,7 +1,10 @@
 package com.myob
 
 import com.myob.CollectionsFp.doubleOf
+import com.myob.CollectionsFp.evenOf
+import com.myob.CollectionsFp.oddOf
 import com.myob.CollectionsFp.sumOf
+import com.myob.CollectionsFp.tripleOf
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 import java.math.BigDecimal
@@ -22,6 +25,18 @@ class CollectionsFpSpec : WordSpec({
 
         "double a list" {
             doubleOf(listOf(1, 2, 3)) shouldBe listOf(2, 4, 6)
+        }
+
+        "triple a list" {
+            tripleOf(listOf(1, 2, 3)) shouldBe listOf(3, 6, 9)
+        }
+
+        "get evens from a list" {
+            evenOf(listOf(1, 2, 3)) shouldBe listOf(2)
+        }
+
+        "get odds from a list" {
+            oddOf(listOf(1, 2, 3)) shouldBe listOf(1, 3)
         }
     }
 })

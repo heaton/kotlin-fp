eaebcc495d3b935d28be20e1e076f92823b7f74c
heaton.cai <heaton.cai@myob.com>
2021-11-26 16:29:17 +1100

    implement an immutable list

---
+++ app/src/main/kotlin/me/heaton/ImList.kt
@@ -0,0 +1,58 @@
+package me.heaton
+
+import java.util.NoSuchElementException
+
+sealed interface ImList<T> {
+    fun head(): T
+    fun tail(): ImList<T>
+    fun <R> fold(acc: R, f: (R, T) -> R): R
+    fun <R> foldRight(acc: R, f: (R, T) -> R): R
+}
+
+class EmptyImList<T> : ImList<T> {
+    override fun head(): T {
+        throw NoSuchElementException("it is empty")
+    }
+
+    override fun tail(): ImList<T> {
+        throw NoSuchElementException("it is empty")
+    }
+
+    override fun <R> fold(acc: R, f: (R, T) -> R): R = acc
+    override fun <R> foldRight(acc: R, f: (R, T) -> R): R = acc
+}
+
+class NonEmptyImList<T>(private val e: T, private val tail: ImList<T>) : ImList<T> {
+    override fun head(): T = e
+    override fun tail(): ImList<T> = tail
+
+    override fun <R> fold(acc: R, f: (R, T) -> R): R = tail.fold(f(acc, e), f)
+    override fun <R> foldRight(acc: R, f: (R, T) -> R): R = f(tail.foldRight(acc, f), e)
+}
+
+fun <T> emptyImList(): ImList<T> = EmptyImList()
+
+fun <T> imListOf(vararg es: T): ImList<T> = es.toList().foldRight(emptyImList()) { e, acc -> acc.ahead(e) }
+
+infix fun <T> ImList<T>.ahead(e: T): ImList<T> = NonEmptyImList(e, this)
+
+fun <T> ImList<T>.reverse(): ImList<T> = fold(emptyImList(), ImList<T>::ahead)
+
+fun <T> ImList<T>.toList(): List<T> = fold(emptyList(), List<T>::plus)
+
+fun <T> ImList<T>.toSet(): Set<T> = fold(emptySet(), Set<T>::plus)
+
+infix fun <T, R> ImList<T>.map(f: (T) -> R): ImList<R> = foldRight(emptyImList()) { acc, e -> acc ahead f(e) }
+
+infix fun <T> ImList<T>.filter(f: (T) -> Boolean): ImList<T> =
+    foldRight(emptyImList()) { acc, e -> if (f(e)) acc ahead e else acc }
+
+operator fun <T> ImList<T>.plus(other: ImList<T>) = foldRight(other, ImList<T>::ahead)
+
+operator fun <T> ImList<T>.minus(other: ImList<T>) = other.toSet().let { others -> filter { !others.contains(it) } }
+
+infix fun <T> ImList<T>.intersect(other: ImList<T>) = filter(other.toSet()::contains)
+
+fun <T> ImList<ImList<T>>.flatten() = fold(emptyImList(), ImList<T>::plus)
+
+infix fun <T> ImList<T>.flatMap(f: (T) -> ImList<T>) = map(f).flatten()
---
+++ app/src/test/kotlin/me/heaton/ImListSpec.kt
@@ -0,0 +1,59 @@
+package me.heaton
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+
+class ImListSpec : WordSpec({
+    "Immutable list" should {
+        "return head" {
+            imListOf(1, 2).head() shouldBe 1
+            imListOf(1, 2).tail().head() shouldBe 2
+        }
+
+        "toList" {
+            imListOf(1, 2, 1, 3).toList() shouldBe listOf(1, 2, 1, 3)
+        }
+
+        "toSet" {
+            imListOf(1, 2, 1, 3).toSet() shouldBe setOf(1, 2, 3)
+        }
+
+        "reverse" {
+            imListOf(1, 2, 3).reverse().toList() shouldBe listOf(3, 2, 1)
+        }
+
+        "map" {
+            imListOf(1, 2, 3).map { it * 2 }.toList() shouldBe listOf(2, 4, 6)
+        }
+
+        "filter" {
+            imListOf(1, 2, 3).filter { it % 2 != 0 }.toList() shouldBe listOf(1, 3)
+        }
+
+        "union 2 lists by +" {
+            (imListOf(1, 2, 3) + imListOf(4, 5, 6)).toList() shouldBe listOf(1, 2, 3, 4, 5, 6)
+        }
+
+        "remove elements by -" {
+            (imListOf(1, 2, 3) - imListOf(1, 2)).toList() shouldBe listOf(3)
+        }
+
+        "return the shared element" {
+            (imListOf(1, 2, 3) intersect imListOf(1, 2, 4)).toList() shouldBe listOf(1, 2)
+        }
+
+        "flat the list" {
+            imListOf(
+                imListOf(1, 2),
+                imListOf(3),
+                imListOf(4, 5, 6)
+            ).flatten().toList() shouldBe listOf(1, 2, 3, 4, 5, 6)
+        }
+
+        "flat map" {
+            imListOf(1, 2).flatMap { x ->
+                imListOf(3, 4).map { y -> x * y }
+            }.toList() shouldBe listOf(3, 4, 6, 8)
+        }
+    }
+})
